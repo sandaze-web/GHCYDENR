f97b93f8a2e5037fc377c550656b796d67d10b8f
Andrey Belkin <belkin00733@gmail.com>
2024-12-12 17:06:53 +0200

    netlify

--- src/components/ProductList/Product/Product.jsx
+++ src/components/ProductList/Product/Product.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Button from "../../Button/Button";
 
-const Product = () => {
+const Product = ({product, className, onAdd}) => {
+    const onAddHandler = () => {
+        onAdd(product);
+    }
+
     return (
-        <div>
-            Product
+        <div className={'product ' + className}>
+            <div className={'img'}/>
+            <div className={'title'}>{product.title}</div>
+            <div className={'description'}>{product.description}</div>
+            <div className={'price'}>
+                <span>Стоимость: <b>{product.price}</b></span>
+            </div>
+            <Button className={'add-btn'} onClick={onAddHandler}>
+                Добавить в корзину
+            </Button>
         </div>
     );
 };
